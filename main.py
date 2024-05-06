10c010719b472dacec7d5fe70bf76cd79088dc4d
IvanZhen6 <145475860+bobcob@users.noreply.github.com>
2024-05-06 10:58:19 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("Hello World!")
+import pygame
+from Test import Test
+
+pygame.init()
+pygame.font.init()
+my_font = pygame.font.SysFont('Arial', 15)
+pygame.display.set_caption("")
+size = (1000, 600)
+screen = pygame.display.set_mode(size)
+
+test = Test(50,50)
+
+
+run = True
+
+while run:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    screen.fill((0, 0, 0))
+
+    screen.blit(test.image, test.rect)
+    pygame.display.update()
+
+# Once we have exited the main program loop we can stop the game engine:
+pygame.quit()
+
